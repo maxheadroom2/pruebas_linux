f2f509c9deb90d067e6ed7075245c7a047dbd217
sinteticbeat@hotmail.com <sinteticbeat@hotmail.com>
2020-05-28 10:14:06 -0500

    ajusto tiempo de notify y añado &&

--- initi.sh
+++ initi.sh
@@ -60,15 +60,15 @@ wmctrl -r "cava" -t 4
 wmctrl -r "mplayer" -t 4
 wmctrl -r "glmark2" -t 3
 wmctrl -r "glmark2" -e 10,276,117,800,600
-sleep 2; &&
+sleep 2s;
 # ciclo para realizar las capturas de pantalla de las estaciones
 for i in 1 2 3 4 5
 do
   wmctrl -s $i &
   notify-send "Captura" "captura de estación de trabajo $i" -i minitube -t 2500 &&
-  sleep 2s;
+  sleep 2s
   scrot -m $rutaH"$i".png &&
-  sleep 1.8;
+  sleep 1.8s
 
 done
 wmctrl -s 0
