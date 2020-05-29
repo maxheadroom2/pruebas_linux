f96ce8a40563c00e3d9dd04b852911686b584662
sinteticbeat@hotmail.com <sinteticbeat@hotmail.com>
2020-05-29 10:38:25 -0500

    añado codigo a yad

--- yad.sh
+++ yad.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-fnUpdateFieldsSBR() {
-    echo "3:${title:-MR}"
-    echo "4:${lastName:-Smith}"
-    echo "5:${gender:-Male}"
-    echo "6:${ssn:-123456789}"
 
-    echo "11:${firstName:-John}"
-    echo "12:${suffix:-III}"
-    echo "13:${dob:-10/10/1978}"
-}
-export -f fnUpdateFieldsSBR
 
 yad \
 --center \
@@ -23,23 +13,20 @@ yad \
 --columns=2 \
 --date-format="%m/%d/%Y" \
 --align=right \
---field="Enterprise Unique ID" "${USAPatXEUIDX:-0001234567}" \
---field="Informacion de Hardware:LBL" "" \
+--field="Informacion de Hardware:LBL" ok "" \
 --field="Modelo:RO" "" \
 --field="Serie:RO" "" \
 --field="Disco Duro:RO" "" \
 --field="Memoria Ram:RO" "" \
---field="Options:LBL" "" \
---field="Show XML Request?:CHK" "FALSE" \
+--field="Detalles particiones:LBL" "" \
  \
---field "  Search!gtk-find:FBTN" "@bash -c \"fnUpdateFieldsSBR \"%16\" \"%8\" \"%1\"  \" " \
 --field=":LBL" "" \
 --field="Procesador:RO" "" \
 --field="Tecnico:RO" "" \
 --field="Modelo de Motherboard:RO" "" \
 --field=".:LBL" "." \
 --field=":LBL" "" \
---field="Show XML Response?:CHK" "FALSE" \
+
  \
 --dialog-sep \
 --button="Quit!gtk-quit:0"
