46de9298f8a06153e7815941c66d567c678b942a
sinteticbeat@hotmail.com <sinteticbeat@hotmail.com>
2020-05-29 10:54:25 -0500

    añado codigo a yad

--- yad.sh
+++ yad.sh
@@ -2,7 +2,7 @@
 # -*- ENCODING: UTF-8 -*-
 
 fnUpdateFieldsSBR() {
-    echo "2:${modelo:-MR}"
+    echo "3:${title:-MR}"
     echo "4:${lastName:-Smith}"
     echo "5:${gender:-Male}"
     echo "6:${ssn:-123456789}"
@@ -10,34 +10,37 @@ fnUpdateFieldsSBR() {
     echo "11:${firstName:-John}"
     echo "12:${suffix:-III}"
     echo "13:${dob:-10/10/1978}"
-    notify-send "ok"
+    notify-send "Inicia LSHW y busca llave OEM"
 }
 export -f fnUpdateFieldsSBR
 
 yad \
 --center \
---title="Caracteristicas" \
---text="<span size=\"xx-large\">Caracteristicas Equipo de Computo</span>\n" \
+--title="Find Patient by EUID" \
+--text="<span size=\"xx-large\">Find Patient Details by EUID</span>\n" \
 --form \
 --width=550 \
 --borders=5 \
 --columns=2 \
 --date-format="%m/%d/%Y" \
 --align=right \
---field="Informacion de Hardware:LBL" "" \
---field="Modelo:RO"" """ \
---field="Serie:RO" "" \
---field="Disco Duro:RO" "" \
---field="Memoria Ram:RO" "" \
---field="Detalles particiones:LBL" "" \
+--field="Enterprise Unique ID" "${USAPatXEUIDX:-0001234567}" \
+--field="Demographics:LBL" "" \
+--field="Title:RO" "" \
+--field="Last Name:RO" "" \
+--field="Sex:RO" "" \
+--field="Social Security Number:RO" "" \
+--field="Options:LBL" "" \
+--field="Show XML Request?:CHK" "FALSE" \
  \
---field "  Actualizar!gtk-find:FBTN" "@bash -c \"fnUpdateFieldsSBR \"%16\" \"%8\" \"%1\"  \" " \
+--field "  Search!gtk-find:FBTN" "@bash -c \"fnUpdateFieldsSBR \"%16\" \"%8\" \"%1\"  \" " \
 --field=":LBL" "" \
---field="Procesador:RO" "" \
---field="Tecnico:RO" "" \
---field="Modelo de Motherboard:RO" "" \
+--field="First Name:RO" "" \
+--field="Suffix:RO" "" \
+--field="Date of Birth:RO" "" \
 --field=".:LBL" "." \
 --field=":LBL" "" \
-
+--field="Show XML Response?:CHK" "FALSE" \
  \
 --dialog-sep \
+--button="Quit!gtk-quit:0"
